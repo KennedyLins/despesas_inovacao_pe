ad2fd63c4a4ad35e239e3c25389ea4d5ba0c4c2f
Kennedy Lins <kennedy.lins@gmail.com>
2023-08-01 15:38:14 -0300

    add sidebar

--- myApp.py
+++ myApp.py
@@ -2,22 +2,45 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
 gastos20 = pd.read_csv('data/despesas_pe_2020.csv', sep=';')
-
 gastos21 = pd.read_csv('data/despesas_pe_2021.csv', sep=';')
-
 gastos22 = pd.read_csv('data/despesas_2022.csv', sep=';')
 
 
-print('Gastos 2020 ---------------------------------')
+selectPage = st.sidebar.selectbox(
+    "O que voce esta procurando?",
+    ("Home", "Analise Exploratoria", "Machine Learning")
+)
 
-print(gastos20.head())
 
-print('Gastos 2021 ---------------------------------')
+if selectPage == "Home":
+    st.write('Home')
 
-print(gastos21.head())
+#Analise Exploratoria
+elif selectPage == "Analise Exploratoria":
+    
+    st.write('Analise Exploratoria')
 
-print('Gastos 2022 ---------------------------------')
+    ano = st.radio(
+    "Escolha um ano para a analise",
+    ('2020', '2021', '2022', 'Todos'))
 
-print(gastos22.head())
+    if ano == '2020':  
+        st.write(gastos20)
+    elif ano == '2021':
+        st.write(gastos21)
+    elif ano == '2022':
+        st.write(gastos22)
+    else:
+        st.write('Concat')
+
+#Machine Learning
+else:
+    st.write('Machine Learning')
+
+
+
+
+
 
